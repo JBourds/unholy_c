eb16fd7b5b1e87ce978893cd3276eea3c55cc4b8
kyserd <kyserdy@gmail.com>
2024-12-13 16:36:50 -0500

    Added compiler driver

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,69 @@
 mod lexer;
 
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+use anyhow::{bail, ensure, Context, Result};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    file: String,
+
+    #[arg(long)]
+    lex: bool,
+
+    #[arg(long)]
+    parse: bool,
+
+    #[arg(long)]
+    codegen: bool,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let file = args.file.clone();
+
+    if !std::fs::exists(&file).with_context(|| format!("Cehcking file \"{}\" failed", file))? {
+        bail!("File \"{}\" does not exist", file);
+    }
+
+    // Step 1: Run the preprocesser on the source file
+    let preproccesed_file = format!("{}.i", file);
+
+    let output = Command::new("gcc")
+        .args(["-E", "-P", &file, "-o", &preproccesed_file])
+        .output()
+        .context("Failed to run gcc from preprocessing")?;
+
+    ensure!(
+        output.status.success(),
+        "gcc exited with error code {}",
+        output.status,
+    );
+
+    // Step 2: Compile the preprocessed source file
+
+    let contents = std::fs::read_to_string(&preproccesed_file)
+        .with_context(|| format!("Failed to read file: \"{}\"", preproccesed_file))?;
+
+    // Cleanup
+    std::fs::remove_file(&preproccesed_file)
+        .with_context(|| format!("Failed to remove temp file \"{}\"", preproccesed_file))?;
+
+    // Lex
+    let tokens = lexer::Lexer::lex(&contents)?;
+
+    if args.lex {
+        println!("Parsed tokens: {:?}", tokens);
+        return Ok(());
+    }
+
+    // Parse
+
+    // Codegen
+
+    // Step 3: Assemble and link the file
+
+    Ok(())
 }

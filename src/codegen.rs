5005fb5a952a24839a9b58e6b50801c8bfe071ab
kyserd <kyserdy@gmail.com>
2024-12-15 16:02:20 -0500

    Added codegen backend

---
+++ src/codegen.rs
@@ -0,0 +1,121 @@
+use crate::{ast, lexer};
+use anyhow::{bail, Result};
+
+pub fn gen<'a>(program: &'a ast::Program<'a>) -> Result<Program<'a>> {
+    let prog = Program::consume(program)?;
+    assert!(prog.len() == 1);
+    Ok(prog.into_iter().next().unwrap())
+}
+
+pub trait AsmNode<'a> {
+    type T;
+
+    fn consume(node: &'a Self::T) -> Result<Vec<Self>>
+    where
+        Self: Sized;
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Program<'a> {
+    pub function: Function<'a>,
+}
+
+impl<'a> AsmNode<'a> for Program<'a> {
+    type T = ast::Program<'a>;
+
+    fn consume(node: &'a Self::T) -> Result<Vec<Self>>
+    where
+        Self: Sized,
+    {
+        let func = Function::consume(&node.function)?;
+
+        assert!(func.len() == 1);
+
+        Ok(vec![Program {
+            function: func.into_iter().next().unwrap(),
+        }])
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Function<'a> {
+    pub name: &'a str,
+    pub instructions: Vec<Instruction>,
+}
+
+impl<'a> AsmNode<'a> for Function<'a> {
+    type T = ast::Function<'a>;
+
+    fn consume(node: &'a Self::T) -> Result<Vec<Self>>
+    where
+        Self: Sized,
+    {
+        let instructions: Result<Vec<Instruction>> = node
+            .body
+            .iter()
+            .map(Instruction::consume)
+            .try_fold(vec![], |mut unrolled, result| {
+                unrolled.extend(result?);
+                Ok(unrolled)
+            });
+
+        Ok(vec![Function {
+            name: match node.name {
+                lexer::Token::Ident(s) => s,
+                _ => bail!("Got a function with a non Ident name: {:?}", node.name),
+            },
+            instructions: instructions?,
+        }])
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Instruction {
+    Mov { src: Operand, dst: Operand },
+    Ret,
+}
+
+impl<'a> AsmNode<'a> for Instruction {
+    type T = ast::Stmt<'a>;
+
+    fn consume(node: &'a Self::T) -> Result<Vec<Self>>
+    where
+        Self: Sized,
+    {
+        match node {
+            ast::Stmt::Return(expr) => match expr {
+                ast::Expr::Literal(e) => {
+                    let ops = Operand::consume(&e)?;
+                    assert!(ops.len() == 1);
+                    Ok(vec![
+                        Instruction::Mov {
+                            src: ops.into_iter().next().unwrap(),
+                            dst: Operand::Register,
+                        },
+                        Instruction::Ret,
+                    ])
+                }
+            },
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Operand {
+    Imm(i32),
+    Register,
+}
+
+impl<'a> AsmNode<'a> for Operand {
+    type T = ast::Literal<'a>;
+
+    fn consume(node: &'a Self::T) -> Result<Vec<Self>>
+    where
+        Self: Sized,
+    {
+        match node {
+            &ast::Literal::Int(i) => Ok(vec![Operand::Imm(i)]),
+            &ast::Literal::String(_) => bail!("Strings are unsupported"),
+        }
+    }
+}

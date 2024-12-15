ff03f9423b6ccae0829918743564bd63c183ac79
kyserd <kyserdy@gmail.com>
2024-12-15 17:08:45 -0500

    Added executable generations and reformatted main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod ast;
 mod codegen;
 mod lexer;
 
-use std::process::Command;
+use std::{ffi::OsStr, io::Write, process::Command};
 
 use anyhow::{bail, ensure, Context, Result};
 use clap::Parser;
@@ -30,27 +30,44 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let file = args.file.clone();
+    // Step 1: Run the preprocesser on the source file
+    let contents = preprocess(&args)?;
 
-    if !std::fs::exists(&file).with_context(|| format!("Checking file \"{}\" failed", file))? {
-        bail!("File \"{}\" does not exist", file);
+    // Step 2: Compile the preprocessed source file
+    let Some((_, _, asm_nodes)) = lex_parse_codegen(&args, contents)? else {
+        return Ok(());
+    };
+
+    // Step 2.5: Turn AsmNodes to asm text
+    let Some(asm) = gen_asm::<std::string::String, asm::x64::Generator>(&args, &asm_nodes)? else {
+        return Ok(());
+    };
+
+    // Step 3: Assemble and link the file
+    assemble_and_link(&args, &asm)?;
+
+    Ok(())
+}
+
+fn preprocess(args: &Args) -> Result<&'static str> {
+    if !std::fs::exists(&args.file)
+        .with_context(|| format!("Checking file \"{}\" failed", args.file))?
+    {
+        bail!("File \"{}\" does not exist", args.file);
     }
 
-    // Step 1: Run the preprocesser on the source file
-    let preproccesed_file = format!("{}.i", file);
+    let preproccesed_file = format!("{}.i", args.file);
 
     let output = Command::new("gcc")
-        .args(["-E", "-P", &file, "-o", &preproccesed_file])
+        .args(["-E", "-P", &args.file, "-o", &preproccesed_file])
         .output()
-        .context("Failed to run gcc from preprocessing")?;
+        .context("Failed to run gcc for preprocessing")?;
 
     ensure!(
         output.status.success(),
         "gcc exited with error code {}",
         output.status,
     );
-
-    // Step 2: Compile the preprocessed source file
 
     let contents = std::fs::read_to_string(&preproccesed_file)
         .with_context(|| format!("Failed to read file: \"{}\"", preproccesed_file))?;
@@ -59,38 +76,103 @@ fn main() -> Result<()> {
     std::fs::remove_file(&preproccesed_file)
         .with_context(|| format!("Failed to remove temp file \"{}\"", preproccesed_file))?;
 
+    Ok(contents.leak())
+}
+
+fn lex_parse_codegen(
+    args: &Args,
+    contents: &'static str,
+) -> Result<
+    Option<(
+        &'static [lexer::Token<'static>],
+        &'static ast::Program<'static>,
+        &'static codegen::Program<'static>,
+    )>,
+> {
     // Lex
-    let tokens = lexer::Lexer::lex(&contents)?;
+    let tokens: &'static [lexer::Token<'static>] =
+        Box::leak(Box::new(lexer::Lexer::lex(&contents)?));
 
     if args.lex {
         println!("Lexed tokens:\n{:#?}", tokens);
-        return Ok(());
+        return Ok(None);
     }
 
     // Parse
-    let ast = ast::parse(&tokens)?;
+    let ast: &'static ast::Program<'static> = Box::leak(Box::new(ast::parse(tokens)?));
     if args.parse {
         println!("Parsed AST:\n{:#?}", ast);
-        return Ok(());
+        return Ok(None);
     }
 
     // Codegen
-    let asm_nodes = codegen::gen(&ast)?;
+    let asm: &'static codegen::Program<'static> = Box::leak(Box::new(codegen::gen(&ast)?));
     if args.codegen {
-        println!("Generated asm nodes:\n{:#?}", asm_nodes);
-        return Ok(());
+        println!("Generated asm nodes:\n{:#?}", asm);
+        return Ok(None);
     }
 
-    // Step 2.5: Turn AsmNodes to asm text
-    let mut asm = String::new();
+    Ok(Some((tokens, ast, asm)))
+}
 
-    asm::x64::Generator::gen(&mut asm, &asm_nodes)?;
+fn gen_asm<W: std::fmt::Write, T: AsmGen<'static, W>>(
+    args: &Args,
+    asm: &'static codegen::Program<'static>,
+) -> Result<Option<String>> {
+    let mut asm_txt = String::new();
+
+    asm::x64::Generator::gen(&mut asm_txt, &asm)?;
 
     if args.asm {
-        println!("Generated asm:\n{asm}");
-        return Ok(());
+        println!("Generated asm:\n{asm_txt}");
+        return Ok(None);
     }
-    // Step 3: Assemble and link the file
+
+    Ok(Some(asm_txt))
+}
+
+fn assemble_and_link(args: &Args, asm: &str) -> Result<()> {
+    let asm_path = std::path::Path::new(&args.file).with_extension(".S");
+    // Ensure that the file is closed
+    {
+        let mut asm_file = std::fs::File::create(&asm_path).with_context(|| {
+            format!(
+                "Could not create assembly file: \"{}\" ",
+                asm_path.to_string_lossy()
+            )
+        })?;
+
+        asm_file.write(asm.as_bytes()).with_context(|| {
+            format!(
+                "Failed to write assembly to \"{}\"",
+                asm_path.to_string_lossy()
+            )
+        })?;
+    }
+
+    let output_name = std::path::Path::new(&args.file).with_extension("");
+
+    let output = Command::new("gcc")
+        .args([
+            asm_path.as_os_str(),
+            OsStr::new("-o"),
+            output_name.as_os_str(),
+        ])
+        .output()
+        .context("Failed to run gcc for assembly and linking")?;
+
+    ensure!(
+        output.status.success(),
+        "gcc exited with error code {}",
+        output.status,
+    );
+
+    std::fs::remove_file(&asm_path).with_context(|| {
+        format!(
+            "Failed to remove temp file \"{}\"",
+            asm_path.to_string_lossy()
+        )
+    })?;
 
     Ok(())
 }

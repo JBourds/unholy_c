f97f71cc7377270bbeea1d7919c234c20a7c91f3
kyserd <kyserdy@gmail.com>
2025-01-16 13:50:59 -0500

    Fix and refactor goto sema to handle compound stmts

--- src/sema.rs
+++ src/sema.rs
@@ -221,41 +221,9 @@ mod gotos {
 
         let mut label_map = HashMap::new();
         let block = if let Some(block) = block {
-            let mut new_blocks = Vec::with_capacity(block.items().len());
-
-            let mut last_thing_was_a_label = (false, false);
-            for block_item in block.into_items().into_iter() {
-                last_thing_was_a_label = match block_item {
-                    ast::BlockItem::Stmt(ast::Stmt::Label(_)) => (last_thing_was_a_label.1, true),
-                    _ => (last_thing_was_a_label.1, false),
-                };
-
-                let fixed = match block_item {
-                ast::BlockItem::Stmt(stmt) => ast::BlockItem::Stmt(resolve_gotos_stmt(
-                    &name,
-                    stmt,
-                    &mut label_map,
-                )?),
-                _ if last_thing_was_a_label.0 => bail!("A label most be followed by a statement, not expression. To get around this, add a null statement i.e: (label: ;)"),
-                _ => block_item.clone(),
-            };
-                new_blocks.push(fixed);
-            }
-            if last_thing_was_a_label.1 {
-                bail!("Label must be followed by a statement. To get around this, add a null statement i.e: (label: ;)")
-            }
-
-            let mut new_blocks1 = Vec::with_capacity(new_blocks.len());
-            for block_item in new_blocks.into_iter() {
-                let fixed = match block_item {
-                    ast::BlockItem::Stmt(stmt) => {
-                        ast::BlockItem::Stmt(validate_goto_stmt(stmt, &label_map)?)
-                    }
-                    _ => block_item.clone(),
-                };
-                new_blocks1.push(fixed);
-            }
-            Some(ast::Block(new_blocks1))
+            let block = resolve_block(block, &name, &mut label_map)?;
+            let block = validate_block(block, &label_map)?;
+            Some(block)
         } else {
             None
         };
@@ -270,23 +238,56 @@ mod gotos {
         })
     }
 
-    pub fn resolve_gotos_stmt(
+    pub fn resolve_block(
+        block: ast::Block,
         func_name: &Rc<String>,
+        label_map: &mut HashMap<Rc<String>, Rc<String>>,
+    ) -> Result<ast::Block> {
+        let mut new_blocks = Vec::with_capacity(block.items().len());
+
+        let mut last_thing_was_a_label = (false, false);
+        for block_item in block.into_items().into_iter() {
+            last_thing_was_a_label = match block_item {
+                ast::BlockItem::Stmt(ast::Stmt::Label(_)) => (last_thing_was_a_label.1, true),
+                _ => (last_thing_was_a_label.1, false),
+            };
+
+            let fixed = match block_item {
+                ast::BlockItem::Stmt(stmt) => ast::BlockItem::Stmt(resolve_stmt(
+                    stmt,
+                    func_name,
+                    label_map,
+                )?),
+                _ if last_thing_was_a_label.0 => bail!("A label most be followed by a statement, not expression. To get around this, add a null statement i.e: (label: ;)"),
+                _ => block_item,
+            };
+            new_blocks.push(fixed);
+        }
+        if last_thing_was_a_label.1 {
+            bail!("Label must be followed by a statement. To get around this, add a null statement i.e: (label: ;)")
+        }
+
+        Ok(ast::Block(new_blocks))
+    }
+
+    pub fn resolve_stmt(
         stmt: ast::Stmt,
+        func_name: &Rc<String>,
         label_map: &mut HashMap<Rc<String>, Rc<String>>,
     ) -> Result<ast::Stmt> {
         match stmt {
+            ast::Stmt::Compound(block) => Ok(ast::Stmt::Compound(resolve_block(
+                block, func_name, label_map,
+            )?)),
             ast::Stmt::If {
                 condition,
                 then,
                 r#else,
             } => Ok(ast::Stmt::If {
                 condition: condition.clone(),
-                then: Box::new(resolve_gotos_stmt(func_name, *then, label_map)?),
+                then: Box::new(resolve_stmt(*then, func_name, label_map)?),
                 r#else: match r#else {
-                    Some(r#else) => {
-                        Some(Box::new(resolve_gotos_stmt(func_name, *r#else, label_map)?))
-                    }
+                    Some(r#else) => Some(Box::new(resolve_stmt(*r#else, func_name, label_map)?)),
                     None => None,
                 },
             }),
@@ -303,20 +304,42 @@ mod gotos {
         }
     }
 
-    pub fn validate_goto_stmt(
+    pub fn validate_block(
+        block: ast::Block,
+        label_map: &HashMap<Rc<String>, Rc<String>>,
+    ) -> Result<ast::Block> {
+        let mut blockitems = Vec::with_capacity(block.items().len());
+
+        for block_item in block.into_items().into_iter() {
+            let fixed = match block_item {
+                ast::BlockItem::Stmt(stmt) => {
+                    ast::BlockItem::Stmt(validate_stmt(stmt, &label_map)?)
+                }
+                _ => block_item.clone(),
+            };
+            blockitems.push(fixed);
+        }
+
+        Ok(ast::Block(blockitems))
+    }
+
+    pub fn validate_stmt(
         stmt: ast::Stmt,
         label_map: &HashMap<Rc<String>, Rc<String>>,
     ) -> Result<ast::Stmt> {
         match stmt {
+            ast::Stmt::Compound(block) => {
+                Ok(ast::Stmt::Compound(validate_block(block, label_map)?))
+            }
             ast::Stmt::If {
                 condition,
                 then,
                 r#else,
             } => Ok(ast::Stmt::If {
                 condition: condition.clone(),
-                then: Box::new(validate_goto_stmt(*then, label_map)?),
+                then: Box::new(validate_stmt(*then, label_map)?),
                 r#else: match r#else {
-                    Some(r#else) => Some(Box::new(validate_goto_stmt(*r#else, label_map)?)),
+                    Some(r#else) => Some(Box::new(validate_stmt(*r#else, label_map)?)),
                     None => None,
                 },
             }),

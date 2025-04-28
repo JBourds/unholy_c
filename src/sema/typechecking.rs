fcfbf6a9bf336093aecddd6cbbbd2504d6f5e13d
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-04-27 21:55:17 -0400

    typechecking: Fixed ternary expression case to not just return the result of typechecking the else branch (as it was previously, when typechecking just errored out if something failed to typecheck) and now it correctly returns the implicitly upcasted result.

--- src/sema/typechecking.rs
+++ src/sema/typechecking.rs
@@ -911,9 +911,21 @@ fn typecheck_expr(expr: &ast::Expr, symbols: &mut SymbolTable) -> Result<TypedEx
             then,
             r#else,
         } => {
-            typecheck_expr(condition, symbols)?;
-            typecheck_expr(then, symbols)?;
-            typecheck_expr(r#else, symbols)
+            // TODO: Make two branches have same output type
+            let target = ast::Type::bool();
+            let condition = Box::new(try_implicit_cast(&target, condition, symbols)
+                .context("Unable to implicitly cast ternary expression condition into a boolean value.")?);
+            let TypedExpr { expr: then_expr, r#type: then_type } = typecheck_expr(then, symbols)
+                    .context("Failed to typecheck ternay expression then branch.")?;
+            let TypedExpr { expr: else_expr, r#type: else_type } = typecheck_expr(r#else, symbols)
+                    .context("Failed to typecheck ternay expression else branch.")?;
+            let (then_base, _) = ast::BaseType::lift(then_type.base.clone(), else_type.base)
+                .context("Ternary expression branches evaluate to different types.")?;
+            let common_type = ast::Type { base: then_base, ..then_type.clone() };
+            let then = Box::new(ast::Expr::Cast { target: common_type.clone(), exp: Box::new(then_expr) });
+            let r#else = Box::new(ast::Expr::Cast { target: common_type, exp: Box::new(else_expr) });
+            // Find common type between then and else
+            Ok(TypedExpr { expr: ast::Expr::Conditional { condition, then, r#else }, r#type: then_type })
         }
         ast::Expr::FunCall { name, args } => match symbols.get(name) {
             Some(SymbolEntry {

ca868a723a90698574db491c9bf868cec1d38a54
kyserd <kyrill@kyserd.com>
2026-06-28 14:25:41 -0400

    idk(sema): validation for struct declarations

--- src/sema/typechecking/struct_union_decl.rs
+++ src/sema/typechecking/struct_union_decl.rs
@@ -1,6 +1,9 @@
+use std::collections::HashSet;
+use std::rc::Rc;
+
 use crate::{ast, sema::typechecking::validate_type_specifier};
 
-use super::{SymbolTable, TypeTable, typecheck_block_item};
+use super::{StructOrUnion, SymbolTable, TypeTable, typecheck_block_item};
 
 use anyhow::{Context, Result, ensure};
 
@@ -9,6 +12,41 @@ pub fn typecheck_struct_decl(
     symbols: &mut SymbolTable,
     structs: &mut TypeTable,
 ) -> Result<ast::StructDecl> {
+    if decl.members.is_empty() {
+        if let Some(entry) = structs.get(&decl.tag) {
+            ensure!(
+                entry.tag_type == StructOrUnion::Struct,
+                "struct decl {} redefined with different tag type",
+                decl.tag
+            );
+        }
+        return Ok(decl);
+    }
+
+    // validating struct definition
+    ensure!(
+        structs.get(&decl.tag).is_none(),
+        "redefining already defined struct {}",
+        decl.tag
+    );
+
+    let mut member_entries = HashSet::new();
+    for member in decl.members {
+        ensure!(
+            !member_entries.contains(&member.name),
+            "members cannot have the same name, member: {} struct: {}",
+            member.name,
+            decl.tag
+        );
+        ensure!(
+            member.r#type.is_complete() && member.r#type.is_array_complete(),
+            "member {} cannot have incomplete type {}",
+            member.name,
+            member.r#type
+        );
+        member_entries.insert(Rc::clone(&member.name));
+    }
+
     todo!()
 }
 

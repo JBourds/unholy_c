4323650765a125bbfd95acc70ffd948164e4f43e
kyserd <kyrill@kyserd.com>
2026-06-28 15:22:27 -0400

    sema: add struct definition calculation

--- src/sema/typechecking/struct_union_decl.rs
+++ src/sema/typechecking/struct_union_decl.rs
@@ -1,9 +1,9 @@
 use std::collections::HashSet;
 use std::rc::Rc;
 
-use crate::{ast, sema::typechecking::validate_type_specifier};
+use crate::{ast, sema::typechecking::MemberEntry};
 
-use super::{StructOrUnion, SymbolTable, TypeTable, typecheck_block_item};
+use super::{StructOrUnion, SymbolTable, TypeTable, fixup_type, typecheck_block_item};
 
 use anyhow::{Context, Result, ensure};
 
@@ -30,10 +30,10 @@ pub fn typecheck_struct_decl(
         decl.tag
     );
 
-    let mut member_entries = HashSet::new();
-    for member in decl.members {
+    let mut member_set = HashSet::new();
+    for member in decl.members.iter() {
         ensure!(
-            !member_entries.contains(&member.name),
+            !member_set.contains(&member.name),
             "members cannot have the same name, member: {} struct: {}",
             member.name,
             decl.tag
@@ -44,9 +44,31 @@ pub fn typecheck_struct_decl(
             member.name,
             member.r#type
         );
-        member_entries.insert(Rc::clone(&member.name));
+        member_set.insert(Rc::clone(&member.name));
     }
 
+    let mut members = vec![];
+    let mut member_entries = vec![];
+    let mut struct_size = 0;
+    let mut struct_alignment = 1;
+    for mut member in decl.members.into_iter() {
+        member.r#type = fixup_type(member.r#type, structs);
+        let member_alignment = member.r#type.alignment;
+        let member_size = member.r#type.base.nbytes();
+        let member_offset = round_up(struct_size, member_alignment.into());
+        member_entries.push(MemberEntry {
+            name: Rc::clone(&member.name),
+            r#type: member.r#type.clone(),
+            offset: member_offset,
+        });
+
+        struct_alignment = struct_alignment.max(member_alignment.into());
+        struct_size = member_offset + member_size;
+
+        members.push(member);
+    }
+
+    struct_size = round_up(struct_size, struct_alignment);
     todo!()
 }
 
@@ -56,4 +78,14 @@ pub fn typecheck_union_decl(
     structs: &mut TypeTable,
 ) -> Result<ast::StructDecl> {
     todo!()
+}
+
+fn round_up(size: usize, alignment: usize) -> usize {
+    assert!(alignment != 0);
+    size.checked_add(alignment - 1)
+        .unwrap()
+        .checked_mul(alignment)
+        .unwrap()
+        .checked_div(alignment)
+        .unwrap()
 }

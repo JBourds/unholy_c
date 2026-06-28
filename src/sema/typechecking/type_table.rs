fa63ee9aec8641abacb71e09290ff4af1211aa0f
kyserd <kyrill@kyserd.com>
2026-06-28 14:25:30 -0400

    feat(sema): get for type table

--- src/sema/typechecking/type_table.rs
+++ src/sema/typechecking/type_table.rs
@@ -6,6 +6,31 @@ pub struct TypeTable {
     scopes: Vec<HashMap<Rc<String>, StructEntry>>,
 }
 
+impl TypeTable {
+    pub fn get(&self, key: &Rc<String>) -> Option<&StructEntry> {
+        Self::get_local(&self.scopes, key).or(Self::get_global(&self.global, key))
+    }
+
+    fn get_local<'a>(
+        scopes: &'a Vec<HashMap<Rc<String>, StructEntry>>,
+        key: &Rc<String>,
+    ) -> Option<&'a StructEntry> {
+        for scope in scopes.iter().rev() {
+            if let Some(entry) = scope.get(key) {
+                return Some(entry);
+            }
+        }
+        None
+    }
+
+    fn get_global<'a>(
+        globals: &'a HashMap<Rc<String>, StructEntry>,
+        key: &Rc<String>,
+    ) -> Option<&'a StructEntry> {
+        globals.get(key)
+    }
+}
+
 #[derive(Debug)]
 pub struct StructEntry {
     pub alignment: usize,
